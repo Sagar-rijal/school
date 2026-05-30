41305b35a7ff2ff1bc3f13d958722408ac479be6
Sagar Rijal <rijalsagar344@gmail.com>
2026-05-30 15:47:22 +0530

    some fixes

--- my-app/lib/auth.ts
+++ my-app/lib/auth.ts
@@ -2,7 +2,22 @@
 import { apiRequest } from "./api";
 import { LoginPayload ,RegisterPayload } from "./type";
 
+const USE_MOCK = true;
+
 export function loginUser(loginData: LoginPayload) {
+  if (USE_MOCK) {
+    return {
+      success: true,
+      message: "User login successfully",
+      data: {
+        _id: "1",
+        name: "Admin",
+        email: "admin@test.com",
+        scope: "TENANT",
+        status: "ACTIVE",
+      },
+    };
+  }
   return apiRequest("/auth/login", {
     method: "POST",
     body: loginData,
